ef3d23b4a0167d31b396e5b5862bfd69236c2985
igor-rizak <igor.rizak@ukr.net>
2023-06-25 23:21:01 +0300

    1

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -19,12 +19,3 @@ export const Statistics = ({ title, stats }) => {
 };
 
 
-
-function getRandomColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-}
-  
-
-
